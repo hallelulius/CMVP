9c92f914ddfd82eed0f32f92ed91eca9492d6f7a
hallelulius <hellaeus@gmail.com>
2015-02-11 17:26:44 +0100

    Added new class

---
+++ CMVP/test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMVP
+{
+    class test
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadLine();
+        }
+    }
+}
