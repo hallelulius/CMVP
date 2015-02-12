22533c65bdf5ef5200695c47483a93d4ed44f838
hallelulius <hellaeus@gmail.com>
2015-02-12 17:32:43 +0100

    Added some comments and tested serialport

--- CMVP/Program.cs
+++ CMVP/Program.cs
@@ -15,5 +15,27 @@ namespace CMVP
             mainGUI mainFrame = new mainGUI();
             Application.Run(mainFrame);
         }
+        
+        /**
+        public static void old()
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.Out.WriteLine("Hej Viktor");
+            System.Console.ReadLine();
+            cout("C++ style");
+            counter(10);
+            System.Console.ReadLine();
+        }
+        public static void cout(String str){
+            System.Console.WriteLine(str);
+        }
+
+        public static void counter(int i)
+        {
+            for(int j =0 ; j<=i; j++){
+                System.Console.WriteLine(j);
+            }
+        }
+        
     }
 }
