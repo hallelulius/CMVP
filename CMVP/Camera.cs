6b4d55459dd09570507772b2fec10c83e703be63
Mattias Kuosku <mattias@kuosku.se>
2015-02-15 20:38:52 +0100

    Created Camera class and imported AForge

---
+++ CMVP/Camera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge.Vision.Motion;
+using AForge.Imaging;
+using AForge.Math.Geometry;
+using AForge.Math;
+
+namespace CMVP
+{
+    class Camera
+    {
+    }
+}
